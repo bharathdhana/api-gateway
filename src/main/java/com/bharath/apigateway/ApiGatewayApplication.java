2a83651eb4b751ce5ef7cd0061c6b8c13acdb54b
Bharath Kumar J N <bharathdhanavan2612@gmail.com>
2026-09-06 16:04:37 +0530

    changes

--- src/main/java/com/bharath/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/bharath/apigateway/ApiGatewayApplication.java
@@ -2,17 +2,13 @@ package com.bharath.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-=======
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
->>>>>>> 8f8b7c1 (Initial commit)
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
+     static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
