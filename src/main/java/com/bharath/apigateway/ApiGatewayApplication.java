e6a266a72eb6cb4736e6cfd40a4f31be9820ab94
Bharath Kumar J N <bharathdhanavan2612@gmail.com>
2026-09-06 15:57:51 +0530

    Initial commit

--- src/main/java/com/bharath/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/bharath/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,13 @@ package com.bharath.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 
+=======
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+>>>>>>> 8f8b7c1 (Initial commit)
 @SpringBootApplication
 public class ApiGatewayApplication {
 
